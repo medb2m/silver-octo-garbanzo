f25b326d8913dd29389607cdc2a3b0e0215af58e
medb2m <79202818+medb2m@users.noreply.github.com>
2024-09-28 12:13:10 +0100

    changed server.js origin and env apiURL

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ const httpServer = createServer(app)
 const io = new Server(httpServer, {
     cors: {
         /* origin: ['http://localhost:4200'], */
-        origin: ['*'],
+        origin: ['http://chaabyourid.org'],
         methods: ['GET', 'POST']
     }
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
     production: false,
-    apiUrl: 'http://0.0.0.0:4000/api'
+    apiUrl: 'http://chaabyourid.org:4000/api'
 };
 
 /*
