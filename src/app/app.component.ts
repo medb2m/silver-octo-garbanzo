1c35db9cc090c606af687b7c1f142d63d57dd7e0
medb2m <79202818+medb2m@users.noreply.github.com>
2024-09-28 11:22:15 +0100

    adding rtl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,58 +7,75 @@ import { filter, map } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 
 
-@Component({ selector: 'app-root', templateUrl: 'app.component.html', styleUrls : ['app.component.css'] })
+@Component({ selector: 'app-root', templateUrl: 'app.component.html', styleUrls: ['app.component.css'] })
 export class AppComponent {
-    Role = Role;
-    account?: Account | null;
-    showBackButton = true
-    showNav = true
+  Role = Role;
+  account?: Account | null;
+  showBackButton = true
+  showNav = true
 
-    isRTL = false;
+  isRTL = false;
 
-    constructor(
-        private router : Router, 
-        private accountService : AccountService, 
-        private titleService: Title,
-        private activatedRoute: ActivatedRoute,
-        private translate: TranslateService
-    ){
-      translate.setDefaultLang('en'); // default language
-      this.translate.onLangChange.subscribe((event) => {
-        this.isRTL = event.lang === 'ar';
-      });
-    }
+  constructor(
+    private router: Router,
+    private accountService: AccountService,
+    private titleService: Title,
+    private activatedRoute: ActivatedRoute,
+    private translate: TranslateService
+  ) {
+    translate.setDefaultLang('en'); // default language
+    this.translate.onLangChange.subscribe((event) => {
+      this.isRTL = event.lang === 'ar';
+    });
+  }
 
 
-    ngOnInit(){
-        this.accountService.account.subscribe(x => this.account = x);
-        this.router.events.subscribe( event => {
-            if (event instanceof NavigationEnd){
-                this.showNav = event.urlAfterRedirects !== '/account/login' && this.account!.role !== Role.Admin
-            }
-        })
-
-        // titles 
-        this.router.events.pipe(
-            filter(event => event instanceof NavigationEnd),
-            map(() => this.activatedRoute),
-            map(route => {
-              while (route.firstChild) route = route.firstChild;
-              return route;
-            }),
-            map(route => route.snapshot.data)
-          ).subscribe(data => {
-            if (data['title']) {
-              this.titleService.setTitle(data['title']);
-            }
-          });
-        }
-
-        switchLanguage(lang: string) {
-          this.translate.use(lang);
-        }
-
-        logout() {
-          this.accountService.logout();
+  ngOnInit() {
+    this.accountService.account.subscribe(x => this.account = x);
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNav = event.urlAfterRedirects !== '/account/login' && this.account!.role !== Role.Admin
       }
+    })
+
+    // titles 
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      }),
+      map(route => route.snapshot.data)
+    ).subscribe(data => {
+      if (data['title']) {
+        this.titleService.setTitle(data['title']);
+      }
+    });
+
+    // Subscribe to language changes
+    this.translate.onLangChange.subscribe((event) => {
+      if (event.lang === 'ar') {
+        document.body.classList.add('rtl');
+      } else {
+        document.body.classList.remove('rtl');
+      }
+    });
+
+    // Initial check when the app starts
+    if (this.translate.currentLang === 'ar') {
+      document.body.classList.add('rtl');
+    } else {
+      document.body.classList.remove('rtl');
+    }
+  }
+
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+  }
+
+  logout() {
+    this.accountService.logout();
+  }
+
 }
